6a1dff3ea06117b27853127cfdc233fbba1b4b2a
aleli <alexredd95@gmail.com>
2022-09-19 00:13:29 -0300

    fixed carousel

--- JS/index.js
+++ JS/index.js
@@ -49,17 +49,56 @@ btnAnterior.addEventListener('click', () => {
 // }
 
 // cargarJuegos();
+const renderCarousel = ()=>{
+    const juegosCortos = juegos.slice(0, 5)
+    // juegosCortos.forEach(juego =>{       
+    //     const contenedor = document.getElementById('carouselContainer');
+    //     const item = document.createElement('div');
+    //     item.classList = 'carousel-item';
+    //     item.innerHTML = `
+    //         <img src="${juego.url}" class="d-block w-100" alt="${juego.title}">
+    //         <div class="carousel-caption d-none d-md-block">
+    //         <h5>${juego.title}</h5>
+    //         <p>${juego.description}</p>
+    //     `
+    //     contenedor.appendChild(item)
+    // }) 
+    for(let i = 0; i < juegosCortos.length; i++){
+        const contenedor = document.getElementById('carouselContainer');
+        const item = document.createElement('div');
+        item.classList = 'carousel-item';
+        if(i === 0){
+            item.classList = 'carousel-item active'
+        }
+        // if(juegosCortos[i].description.length < 117){
+        //     let descripcion = juegosCortos[i].description.slice(0, 117);
+        //     console.log(descripcion)
+        // }
+        item.innerHTML = `
+            <img src="${juegosCortos[i].url}" class="d-block w-100" alt="${juegosCortos[i].title}">
+            <div class="carousel-caption d-none d-md-block">
+            <h5>${juegosCortos[i].title}</h5>
+            <p>${juegosCortos[i].description.slice(0,90)}</p>
+        `
+        contenedor.appendChild(item)
+        
+    }
+}
+
+renderCarousel()
 
 const renderGames = ()=>{
     juegos.forEach(juego =>{
         const contenedor = document.getElementById('contenedor');
-        const tarjeta = document.createElement('div')
+        const tarjeta = document.createElement('div');
         tarjeta.innerHTML = `
             <img class="img-fluid" src="${juego.url}">
             <h3 class="title">${juego.title}</h3>        
             `;
-        contenedor.appendChild(tarjeta)
+        contenedor.appendChild(tarjeta);
     })
 }
 
 renderGames()
+
+
