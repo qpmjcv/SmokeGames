65d41727093c8b08328fd43814d537638b89aab5
aleli <alexredd95@gmail.com>
2022-09-19 02:36:36 -0300

    fixed carousel; functional modal; styles added

--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,23 @@
-let pagina = 1;
-const btnAnterior = document.getElementById('btnAnterior');
-const btnSiguiente = document.getElementById('btnSiguiente');
+// let pagina = 1;
+// const btnAnterior = document.getElementById('btnAnterior');
+// const btnSiguiente = document.getElementById('btnSiguiente');
 const juegos = JSON.parse(localStorage.getItem('juegos')) || [];
 
-btnSiguiente.addEventListener('click', () => {
-    if(pagina <1000){
-        pagina += 1;
-        obtenerJuegos();
+// btnSiguiente.addEventListener('click', () => {
+//     if(pagina <1000){
+//         pagina += 1;
+//         obtenerJuegos();
 
-    }
-})
+//     }
+// })
 
-btnAnterior.addEventListener('click', () => {
-    if(pagina > 1){
-        pagina -= 1;
-        obtenerJuegos();
+// btnAnterior.addEventListener('click', () => {
+//     if(pagina > 1){
+//         pagina -= 1;
+//         obtenerJuegos();
 
-    }
-})
+//     }
+// })
 
 // const cargarJuegos = async() => {
 //     try {
@@ -49,6 +49,7 @@ btnAnterior.addEventListener('click', () => {
 // }
 
 // cargarJuegos();
+
 const renderCarousel = ()=>{
     const juegosCortos = juegos.slice(0, 5)
     // juegosCortos.forEach(juego =>{       
@@ -69,19 +70,18 @@ const renderCarousel = ()=>{
         item.classList = 'carousel-item';
         if(i === 0){
             item.classList = 'carousel-item active'
-        }
+        };
         // if(juegosCortos[i].description.length < 117){
         //     let descripcion = juegosCortos[i].description.slice(0, 117);
         //     console.log(descripcion)
         // }
         item.innerHTML = `
-            <img src="${juegosCortos[i].url}" class="d-block w-100" alt="${juegosCortos[i].title}">
+            <img src="${juegosCortos[i].url}" class="d-block w-100 img-fluid" alt="${juegosCortos[i].title}">
             <div class="carousel-caption d-none d-md-block">
             <h5>${juegosCortos[i].title}</h5>
-            <p>${juegosCortos[i].description.slice(0,90)}</p>
-        `
-        contenedor.appendChild(item)
-        
+            <p>${juegosCortos[i].description.slice(0, 90)}</p>
+        `;
+        contenedor.appendChild(item);        
     }
 }
 
@@ -99,6 +99,43 @@ const renderGames = ()=>{
     })
 }
 
-renderGames()
+ renderGames()
+
+ const modalRender1 = (() =>{
+     juegos.slice(20, 26).forEach(juego =>{
+         const modalContainer = document.getElementById('modal1')
+         const modalCard = document.createElement('div')
+         modalCard.innerHTML=`
+         <img class="img-fluid" src="${juego.url}">
+         <h3 class="title">${juego.title}</h3>        
+         `;
+         modalContainer.appendChild(modalCard);
+    })
+ })()
+
+const modalRender2 = (() =>{
+    juegos.slice(5, 11).forEach(juego =>{
+        const modalCard = document.createElement('div')
+        const modalContainer = document.getElementById('modal2')
+        modalCard.innerHTML=`
+        <img class="img-fluid" src="${juego.url}">
+        <h3 class="title">${juego.title}</h3>        
+        `;
+        modalContainer.appendChild(modalCard);
+    })
+})()
+
+const modalRender3 = (() =>{
+    juegos.slice(14, 20).forEach(juego =>{
+        const modalCard = document.createElement('div')
+        const modalContainer = document.getElementById('modal3')
+        modalCard.innerHTML=`
+        <img class="img-fluid" src="${juego.url}">
+        <h3 class="title">${juego.title}</h3>        
+        `;
+        modalContainer.appendChild(modalCard);
+    })
+}) ()
+
 
 
